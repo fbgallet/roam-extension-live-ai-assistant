a41f7febfa6d3058166b8dc401f31dfc54605c5c
Fabrice Gallet <74436347+fbgallet@users.noreply.github.com>
2025-01-29 17:08:45 +0100

    Added siblings search & directed filters & searchOnly condition

--- src/utils/dataProcessing.js
+++ src/utils/dataProcessing.js
@@ -1,3 +1,20 @@
+export function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+export function removeDuplicatesByProperty(arr, property) {
+  const seen = new Set();
+  return arr.filter((item) => {
+    const value = item[property];
+    if (seen.has(value)) {
+      return false;
+    } else {
+      seen.add(value);
+      return true;
+    }
+  });
+}
+
 export function excludeItemsInArray(
   sourceArray,
   itemsToExclude,
@@ -40,4 +57,17 @@ export function sliceByWordLimit(text, wordLimit) {
     return text;
   }
   return words.slice(0, wordLimit).join(" ") + "...";
+}
+
+export function getRandomElements(arr, n) {
+  const arrayCopy = [...arr];
+  const result = [];
+  if (n >= arrayCopy.length) return arr;
+  n = Math.min(n, arrayCopy.length);
+  while (result.length < n) {
+    const randomIndex = Math.floor(Math.random() * arrayCopy.length);
+    result.push(arrayCopy[randomIndex]);
+    arrayCopy.splice(randomIndex, 1);
+  }
+  return result;
 }
--- src/utils/roamAPI.js
+++ src/utils/roamAPI.js
@@ -484,17 +484,74 @@ export const cleanFlagFromBlocks = (flag, blockUids) => {
   );
 };
 
-export const getBlocksMatchingRegexQuery = (withExcludeRegex, pageRegex) => {
+const getAdaptativeQueryStrings = (
+  withExcludeRegex,
+  pageRegex,
+  nbOfRegex,
+  onlySiblings
+) => {
+  let regexVarStr = "";
+  let findStr = "";
+  let resultStr = "";
+  let excludeRegexVar = "";
+  let excludeStr = "";
+  let findSiblingsStr = "";
+  if (nbOfRegex) {
+    for (let i = 0; i < nbOfRegex; i++) {
+      resultStr += `?child-uid${i} ?child-content${i} `;
+      regexVarStr += `?regex${i} `;
+      if (!onlySiblings) {
+        findStr += `
+        [(re-pattern ?regex${i}) ?pattern${i}]
+        (descendants ?b ?child${i})
+        [?child${i} :block/uid ?child-uid${i}]
+        [?child${i} :block/string ?child-content${i}]
+        (or
+          [(re-find ?pattern${i} ?child-content${i})]
+          [(re-find ?pattern${i} ?content)])\n`;
+      } else if (i > 1) {
+        findSiblingsStr += `[?parents :block/children ?child${i}]
+    [?child${i} :block/string ?child-content${i}]
+    [(not= ?child${i - 1} ?child${i})]
+    [(re-pattern ?regex${i}) ?pattern${i}]
+    [(re-find ?pattern${i} ?child-content${i})]
+    [?child${i} :block/uid ?child-uid${i}]\n`;
+      }
+    }
+  }
   const pageStr = pageRegex
     ? pageRegex === "dnp"
       ? `[?page :block/uid ?page-uid]
-      [(re-pattern "${dnpUidRegex.source.slice(1, -1)}") ?page-regex]
-  [(re-find ?page-regex ?page-uid)]`
+    [(re-pattern "${dnpUidRegex.source.slice(1, -1)}") ?page-regex]
+[(re-find ?page-regex ?page-uid)]`
       : `[(re-pattern "${pageRegex}") ?page-regex]
-  [(re-find ?page-regex ?page-title)]`
+[(re-find ?page-regex ?page-title)]`
     : "";
+
+  if (withExcludeRegex) {
+    excludeRegexVar = "?regex-not";
+    excludeStr = "[(re-pattern ?regex-not) ?pattern-not]";
+  }
+
+  return {
+    resultStr,
+    regexVarStr,
+    findStr,
+    pageStr,
+    excludeRegexVar,
+    excludeStr,
+    findSiblingsStr,
+  };
+};
+
+export const getBlocksMatchingRegexQuery = (withExcludeRegex, pageRegex) => {
+  const { pageStr, excludeRegexVar, excludeStr } = getAdaptativeQueryStrings(
+    withExcludeRegex,
+    pageRegex
+  );
+
   const q = `[:find ?uid ?content ?time ?page-title
-    :in $ ?regex${withExcludeRegex ? " ?regex-not" : ""}
+    :in $ ?regex ${excludeRegexVar}
     :where
     [?b :block/uid ?uid]
    [?b :block/string ?content]
@@ -505,12 +562,11 @@ export const getBlocksMatchingRegexQuery = (withExcludeRegex, pageRegex) => {
    [(re-pattern ?regex) ?pattern]
    [(re-find ?pattern ?content)]
    ${
-     withExcludeRegex
-       ? `[(re-pattern ?regex-not) ?pattern-not]
+     excludeStr
+       ? `${excludeStr}
       (not [(re-find ?pattern-not ?content)])`
        : ""
    }]`;
-  console.log("q :>> ", q);
   return q;
 };
 
@@ -525,34 +581,18 @@ export const getMultipleMatchingRegexInTreeQuery = (
   withExcludeRegex,
   pageRegex
 ) => {
-  let regexVarStr = "";
-  let findStr = "";
-  let resultStr = "";
-  const pageStr = pageRegex
-    ? pageRegex === "dnp"
-      ? `[?page :block/uid ?page-uid]
-    [(re-pattern "${dnpUidRegex.source.slice(1, -1)}") ?page-regex]
-[(re-find ?page-regex ?page-uid)]`
-      : `[(re-pattern "${pageRegex}") ?page-regex]
-[(re-find ?page-regex ?page-title)]`
-    : "";
-  for (let i = 0; i < nbOfRegex; i++) {
-    resultStr += `?child-uid${i} ?child-content${i} `;
-    regexVarStr += `?regex${i} `;
-    findStr += `
-    [(re-pattern ?regex${i}) ?pattern${i}]
-    (descendants ?b ?child${i})
-    [?child${i} :block/uid ?child-uid${i}]
-    [?child${i} :block/string ?child-content${i}]
-    (or
-      [(re-find ?pattern${i} ?child-content${i})]
-      [(re-find ?pattern${i} ?content)])\n`;
-  }
+  const {
+    resultStr,
+    regexVarStr,
+    findStr,
+    pageStr,
+    excludeRegexVar,
+    excludeStr,
+  } = getAdaptativeQueryStrings(withExcludeRegex, pageRegex, nbOfRegex);
 
   const q = `[:find ?matching-b ?content ?time ?page-title ${resultStr}
-    :in $ % [?matching-b ...] ${regexVarStr}${
-    withExcludeRegex ? " ?regex-not" : ""
-  }
+    :in $ % [?matching-b ...] ${regexVarStr} ${excludeRegexVar}
+    
     :where
     [?b :block/uid ?matching-b]
     [?b :block/string ?content]
@@ -561,8 +601,8 @@ export const getMultipleMatchingRegexInTreeQuery = (
     ${pageStr}
     [?b :edit/time ?time]
     ${
-      withExcludeRegex
-        ? `[(re-pattern ?regex-not) ?pattern-not]
+      excludeStr
+        ? `${excludeStr}
       [?b :block/children ?direct-child]
       [?direct-child :block/string ?child-content]
       (not [(re-find ?pattern-not ?content)])
@@ -570,6 +610,56 @@ export const getMultipleMatchingRegexInTreeQuery = (
         : ""
     }
     ${findStr}
+    ]`;
+  return q;
+};
+
+export const getSiblingsParentMatchingRegexQuery = (
+  nbOfRegex,
+  withExcludeRegex,
+  pageRegex
+) => {
+  const {
+    resultStr,
+    regexVarStr,
+    findStr,
+    pageStr,
+    excludeRegexVar,
+    excludeStr,
+    findSiblingsStr,
+  } = getAdaptativeQueryStrings(withExcludeRegex, pageRegex, nbOfRegex, true);
+
+  const q = `[:find ?parent-uid ?parent-content ?time ?page-title ${resultStr}
+  :in $ [?matching-b ...] ${regexVarStr} ${excludeRegexVar}
+  :where
+    [?b :block/uid ?matching-b]
+    [?b :block/page ?page]
+    [?page :node/title ?page-title]
+    ${pageStr}
+    [?parents :block/children ?b]
+    [?parents :block/children ?child0]
+    [?parents :block/children ?child1]
+    [?child0 :block/string ?child-content0]
+    [?child1 :block/string ?child-content1]
+    [(not= ?child0 ?child1)]
+    ${
+      excludeStr
+        ? `${excludeStr}
+      (not [?parents :block/children ?any-child]
+      [?any-child :block/string ?any-content]
+      [(re-find ?pattern-not ?any-content)])`
+        : ""
+    }
+    [(re-pattern ?regex0) ?pattern0]
+    [(re-pattern ?regex1) ?pattern1]
+    [(re-find ?pattern0 ?child-content0)]
+    [(re-find ?pattern1 ?child-content1)]
+    ${findSiblingsStr}
+    [?child0 :block/uid ?child-uid0]
+    [?child1 :block/uid ?child-uid1]
+    [?parents :block/uid ?parent-uid]
+    [?parents :block/string ?parent-content]
+    [?parents :edit/time ?time]
     ]`;
   return q;
 };
